819d72d98e3f27091f340c32946ed9804a4ced39
V <victoriaakhmatova@Sleipnir.local>
2024-04-16 19:02:00 +0400

    added models, added some basic functionality

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,18 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- project4/network/views.py
+++ project4/network/views.py
@@ -1,14 +1,27 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Like
 
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all().order_by("-created_at")
+    post_likes = 0
+    is_liked_by_user = False
+    for current_post in posts:
+        post_likes = current_post.like_set.count()
+
+        if request.user.is_authenticated:
+            try:
+                Like.objects.get(user=request.user, post=current_post)
+                is_liked_by_user = True
+            except Like.DoesNotExist:
+                is_liked_by_user = False
+    return render(request, "network/index.html", {
+        "posts": posts, "post_likes": post_likes, "is_liked_by_user": is_liked_by_user})
 
 
 def login_view(request):
@@ -61,3 +74,54 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def profile(request, user_id):
+    user = User.objects.get(id=user_id)
+    followers = user.followers.all()
+    following = user.following.all()
+    posts = Post.objects.filter(user=user).order_by("-created_at")
+    return render(request, "network/profile.html", {
+        "user": user,
+        "followers": followers,
+        "following": following,
+        "posts": posts,
+    })
+
+
+def post(request):
+    if request.method == "POST":
+        user = request.user
+        content = request.POST["content"]
+        new_post = Post(user=user, content=content)
+        new_post.save()
+        return HttpResponseRedirect(reverse("index"))
+
+
+def like(request, post_id):
+    if request.method == "POST":
+        new_post = Post.objects.get(id=post_id)
+        user = request.user
+        try:
+            user_like = Like.objects.get(user=user, post=new_post)
+            user_like.delete()
+
+            return JsonResponse({
+                'likes': new_post.like_set.count(),
+                'is_liked_by_user': False
+            })
+        except Like.DoesNotExist:
+            user_like = Like(user=user, post=new_post)
+            user_like.save()
+            return JsonResponse({
+                'likes': new_post.like_set.count(),
+                'is_liked_by_user': True
+            })
+
+
+def edit(request, post_id):
+    if request.method == "PUT":
+        post = Post.objects.get(id=post_id)
+        post.content = request.POST["content"]
+        post.save()
+        return HttpResponseRedirect(reverse("index"))
